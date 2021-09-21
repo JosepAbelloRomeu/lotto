2653efe06f8a6a4d6b293db3d8d1500c21d11c18
Josep Abelló <josep.abello@studiogenesis.es>
2021-09-21 17:04:39 +0200

    MOre changes

--- app/Http/Controllers/HistoricController.php
+++ app/Http/Controllers/HistoricController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Historic;
 use App\Result;
+use App\Team;
 use Illuminate\Http\Request;
 
 class HistoricController extends Controller
@@ -43,12 +44,24 @@ class HistoricController extends Controller
         $prevision = null;
 
         if ($result !== null) {
-            if ($result->wins > $result->loses) {
-                $prevision = '1';
-            } else if ($result->wins < $result->loses) {
-                $prevision = '2';
+            if ($result->wins > 5 || $result->loses > 5) {
+                if ($result->wins > $result->loses) {
+                    $prevision = '1';
+                } else if ($result->wins < $result->loses) {
+                    $prevision = '2';
+                } else {
+                    $prevision = 'X';
+                }
             } else {
-                $prevision = 'X';
+                $teamOne = Team::where('team', $local)->first();
+                $teamTwo = Team::where('team', $visitor)->first();
+                if ($teamOne->wins > $teamTwo->wins) {
+                    $prevision = '1';
+                } else if ($teamOne->wins < $teamTwo->wins) {
+                    $prevision = '2';
+                } else {
+                    $prevision = 'X';
+                }
             }
         } else {
             $prevision = 'X';
